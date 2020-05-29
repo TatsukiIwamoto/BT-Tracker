86f36f921bf2420fdff6410654379d7548084553
TatsukiIwamoto <2020m034@kuas.ac.jp>
2020-05-29 10:44:48 +0900

    Added all files
    Copied all files of BT-Tracker project

---
+++ app/src/main/java/com/example/bt_tracker/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.bt_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToLogActivity(View view) {
+        Intent tolog = new Intent(this, LogActivity.class);
+        startActivity(tolog);
+    }
+
+    public void goToNormalActivity(View view) {
+        Intent toNormal = new Intent(this, NormalActivity.class);
+        startActivity(toNormal);
+    }
+
+    public void goToMechanismActivity(View view) {
+        Intent toMechanism = new Intent(this, MechanismActivity.class);
+        startActivity(toMechanism);
+    }
+}
